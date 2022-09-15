b00e2f0ae3103129ae7b2a380cccd786fe39c043
sterrettJD <sterrettjd@protonmail.com>
2022-09-15 11:49:34 -0600

    update plotting of extracted coefficients

--- rstan-diff-abundance.R
+++ rstan-diff-abundance.R
@@ -11,10 +11,10 @@ gt_data <- read_qza("../../Reisdorph/GT-micro-metabo/microbiome/table_GT.qza")$d
     t() %>% as.data.frame()
 
 # only grab a few of the columns since we're testing stan
-total_reads <- sum(gt_data)
-microbe_read_sums <- colSums(gt_data)
-sum(microbe_read_sums > 0.005*total_reads) # 29 microbes have > 0.5% of the total reads
-gt_data <- gt_data[,which(microbe_read_sums > 0.005*total_reads)]
+# total_reads <- sum(gt_data)
+# microbe_read_sums <- colSums(gt_data)
+# sum(microbe_read_sums > 0.005*total_reads) # 29 microbes have > 0.5% of the total reads
+# gt_data <- gt_data[,which(microbe_read_sums > 0.005*total_reads)]
 
 
 # read in metadata
@@ -42,7 +42,7 @@ D <- ncol(gt_data)
 # Create the predictor vector
 x <- model.matrix(~Germ_free, data=cbind(gt_data, metadata)) %>% 
     as.data.frame()
-p <- ncol(X-1) #subtract 1 bc of the intercept
+p <- ncol(x-1) #subtract 1 bc of the intercept
 
 # calculate sequencing depth for each sample
 depth <- gt_data %>% rowSums()
@@ -64,17 +64,18 @@ stan_data <- list(
 fit1 <- stan(
     file = "diff-abund-NB.stan",  # Stan program
     data = stan_data,    # named list of data
-    chains = 4,             # number of Markov chains
+    chains = 4,# number of Markov chains
     warmup = 500,          # number of warmup iterations per chain
-    iter = 1000,            # total number of iterations per chain
-    cores = 4,              # number of cores (could use one per chain)
-    refresh = 1             # progress shown
+    iter = 2000,            # total number of iterations per chain
+    cores = 4,# number of cores (could use one per chain)
+    refresh = 1, # progress shown
 )
 
 # assess sampler params
 sampler_params <- get_sampler_params(fit1, inc_warmup = TRUE)
 summary(do.call(rbind, sampler_params), digits = 2)
 
+summary(fit1)
 
 # print some of the output params, phi and beta
 print(fit1, pars = c("phi", "beta"))
@@ -98,12 +99,67 @@ model_betas_clr <- alr2clr(model_betas)
 # check that this does the same as using alrInv to clr
 # either of these options should work
 model_betas_clr2 <- compositions::alrInv(model_betas) %>% compositions::clr()
-rbind(model_betas_clr[1,],
-      model_betas_clr2[1,])
+rbind(head(model_betas_clr[1,]),
+      head(model_betas_clr2[1,]))
 
 model_betas_clr <- model_betas_clr %>% as.data.frame()
 colnames(model_betas_clr) <- colnames(gt_data)
 
+# rename the columns with taxonomy
+tax <- read_qza("../../Reisdorph/GT-micro-metabo/microbiome/taxonomy_GT.qza")$data %>%
+    as.data.frame()
+
+model_betas_clr_asv <- model_betas_clr
+colnames(model_betas_clr) <- sapply(
+    colnames(model_betas_clr),
+    function(x){tax[tax$Feature.ID==x,"Taxon"]}
+    )
+
+microbe_beta_means_asv <- colMeans(model_betas_clr_asv)
+microbe_beta_sd_asv <- apply(model_betas_clr_asv, 2, sd)
+
+beta_plot_data <- data.frame(
+    microbe_beta_means_asv, microbe_beta_sd_asv, 
+    row.names=colnames(model_betas_clr_asv))
+
+beta_plot_data$tax <- colnames(model_betas_clr)
+beta_plot_data <- beta_plot_data %>% 
+    rename(mean=microbe_beta_means_asv,
+                          sd=microbe_beta_sd_asv)
+beta_plot_data$nameshort <- beta_plot_data$tax %>% 
+    sapply(function(x){str_split(x, pattern=";", n=4)[[1]][4]})
+beta_plot_data$asv <- rownames(beta_plot_data)
+
+beta_plot_data <- beta_plot_data[order(beta_plot_data$mean),]
+
+pd <- position_dodge(0.1)
+ggplot(beta_plot_data,
+       aes(y=reorder(asv,mean), x=mean)) +
+    geom_point(position=pd, stat="identity",
+             colour='red') +
+    geom_errorbar(aes(xmin=mean-sd, xmax=mean+sd),
+                  position=pd, alpha=0.1) +
+    theme(axis.text.y=element_blank()) +
+    ylab("ASV") +
+    xlab("Log(Germ Free/Control) + K")
+
+head(beta_plot_data)
+tail(beta_plot_data)
 
 
-traceplot(fit1, pars = c("beta"), inc_warmup = TRUE, nrow = 29)
+
+
+
+
+long_betas <- pivot_longer(model_betas_clr, cols = everything())
+long_betas$nameshort <- long_betas$name %>% sapply(function(x){str_split(x, pattern=";", n=4)[[1]][4]})
+
+ggplot(data=long_betas,
+       mapping=aes(y=nameshort, x=value)) +
+    geom_boxplot()
+
+
+
+
+
+
