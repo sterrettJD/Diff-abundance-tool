6f8817988f4b7a8ed78ffd46178336614c06265d
sterrettJD <sterrettjd@protonmail.com>
2022-09-15 08:26:51 -0600

    prep inputs for stan

--- rstan-diff-abundance.R
+++ rstan-diff-abundance.R
@@ -16,6 +16,7 @@ metadata <- data.table::fread("../../Reisdorph/GT-micro-metabo/metadata/mapping.
 
 metadata$SampleID <- metadata$`#SampleID`
 rownames(metadata) <- metadata$SampleID
+metadata$Germ_free <- metadata$PID == "Control"
 
 # do all row names match?
 length(match(rownames(metadata),
@@ -26,6 +27,29 @@ length(match(rownames(metadata),
 metadata <- metadata[rownames(gt_data),]
 
 
+# model inputs
+formula <- "Germ_free"
+N <- nrow(gt_data)
+D <- ncol(gt_data)
 
+# Create the predictor vector
+X <- model.matrix(~Germ_free, data=cbind(gt_data, metadata)) %>% 
+    as.data.frame()
+p <- ncol(X-1) #subtract 1 bc of the intercept
 
-       
+# calculate sequencing depth for each sample
+depth <- gt_data %>% rowSums()
+
+# The count table is our outcome y
+y <- gt_data
+
+#concatenate to a named list for stan
+stan_data <- list(
+    N=N,
+    D=D,
+    p=p,
+    depth=depth,
+    X=X,
+    y=y,
+)
+
